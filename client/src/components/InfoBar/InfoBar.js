e7f79bd09debe38555107d8ac9bcb2dc4605ca21
Lukas Saylor <slappyhoursound@gmail.com>
2020-08-25 14:20:56 -0400

    closing chat bug

--- client/src/components/InfoBar/InfoBar.js
+++ client/src/components/InfoBar/InfoBar.js
@@ -10,7 +10,7 @@ const InfoBar =({room}) => (
             <h3>{room}</h3>
         </div>
         <div className="rightInnerContainer">
-            <a href="/"><img src={closeIcon} alt="close"/></a>
+            <a href="/reactchat/#/"><img src={closeIcon} alt="close"/></a>
         </div>
     </div>
 );
