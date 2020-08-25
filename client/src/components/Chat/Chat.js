33b6cfc9bb834b68ef67db5a74d84b745567fad7
Lukas Saylor <slappyhoursound@gmail.com>
2020-08-25 11:28:24 -0400

    messaging completed

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -4,12 +4,15 @@ import io from "socket.io-client";
 import './Chat.css';
 import InfoBar from '../InfoBar/InfoBar';
 import Input from '../Input/Input';
+import Messages from '../Messages/Messages';
+import TextContainer from '../TextContainer/TextContainer';
 
 let socket;
 
 const Chat = ({ location }) => {
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
+  const [users, setUsers] = useState('');
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
   const ENDPOINT = "localhost:5000";
@@ -34,6 +37,10 @@ const Chat = ({ location }) => {
     socket.on("message", (message) => {
       setMessages([...messages, message]);
     });
+
+    socket.on("roomData", ({ users }) => {
+      setUsers(users);
+    });
   }, [messages]);
 
   const sendMessage = (event) => {
@@ -51,8 +58,10 @@ const Chat = ({ location }) => {
     <div className="outerContainer">
       <div className="container">
         <InfoBar room={room}/>
+        <Messages messages={messages} name={name}/>
         <Input message={message} setMessage={setMessage} sendMessage={sendMessage} /> 
       </div>
+        <TextContainer users={users}/>
     </div>
   )
 };
